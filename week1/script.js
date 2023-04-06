aacf52bfed7b95a7c59eb30373624e7e3b0fdcdf
Replit user <>
2023-04-06 23:10:27 +0000

    added document.write

--- week1/script.js
+++ week1/script.js
@@ -1,3 +1,10 @@
 'use strict'
 
 console.log("hello!")
+
+let firstName = 'Matt';
+    let lastName = 'Miguel';
+    let yearsOfStudy = 3;
+    let goal = 'obtain a job so I can support me and my family comfortably'
+
+    document.write("My name is " + firstName + " " + lastName + ". I have been at SCC for " + yearsOfStudy + " years non-consecutively and plan to " + goal + " after completing my courses.");
