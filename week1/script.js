7e3cb7fd7c4de9565eda992078354fb091a7de91
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-04-08 22:12:33 -0700

    added styling to some pages

--- week1/script.js
+++ week1/script.js
@@ -3,8 +3,10 @@
 console.log("hello!")
 
 let firstName = 'Matt';
-    let lastName = 'Miguel';
-    let yearsOfStudy = 3;
-    let goal = 'obtain a job so I can support me and my family comfortably'
+let lastName = 'Miguel';
+let yearsOfStudy = 3;
+let goal = 'obtain a job so I can support me and my family comfortably'
+let write = document.getElementById('write')
+// document.write("My name is " + firstName + " " + lastName + ". I have been at SCC for " + yearsOfStudy + " years non-consecutively and plan to " + goal + " after completing my courses.");
 
-    document.write("My name is " + firstName + " " + lastName + ". I have been at SCC for " + yearsOfStudy + " years non-consecutively and plan to " + goal + " after completing my courses.");
+write.textContent = "My name is " + firstName + " " + lastName + ". I have been at SCC for " + yearsOfStudy + " years non-consecutively and plan to " + goal + " after completing my courses."
