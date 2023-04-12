1f071f1882f010de0860e9030e03ef7ee3757bee
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-04-12 13:22:39 -0700

    made temp converter from C to F

--- week2/tempConvert/app.js
+++ week2/tempConvert/app.js
@@ -7,7 +7,16 @@ let form = document.querySelector('form')
 function handleSubmit(event) {
   event.preventDefault();
 
-  console.log('inside handle submit', +event.target.temp.value);
+  // console.log('inside handle submit', +event.target.temp.value);
+
+  let temp = +event.target.temp.value;
+  
+  let degFahren = 9 / 5 * temp + 32
+  
+  let faren = document.getElementById('faren')
+  console.log(faren);
+
+  faren.textContent = `You entered ${temp} C! After some magic that comes to ${degFahren} F. Wow, much temperature`
 
 
 }
