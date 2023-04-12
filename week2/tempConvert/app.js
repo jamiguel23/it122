6efb786e676dba61b2cb0f4d9b6112c46b81c331
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-04-12 13:34:50 -0700

    working on error handling

--- week2/tempConvert/app.js
+++ week2/tempConvert/app.js
@@ -9,14 +9,25 @@ function handleSubmit(event) {
 
   // console.log('inside handle submit', +event.target.temp.value);
 
-  let temp = +event.target.temp.value;
-  
-  let degFahren = 9 / 5 * temp + 32
-  
   let faren = document.getElementById('faren')
-  console.log(faren);
 
-  faren.textContent = `You entered ${temp} C! After some magic that comes to ${degFahren} F. Wow, much temperature`
+  let temp = +event.target.temp.value;
+
+  console.log(temp == NaN);
+  if(!temp){
+
+  console.log(temp);
+
+  faren.textContent = "please enter a number"
+  
+  }else{
+    
+    let degFahren = 9 / 5 * temp + 32
+    
+  
+    faren.textContent = `You entered ${temp} C! After some magic that comes to ${degFahren} F. Wow, much temperature`
+  }
+  
 
 
 }
