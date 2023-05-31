260d51b557b07e1bd29682688ee2cf6a672efaed
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-05-31 10:19:51 -0700

    worht on assignement

--- week9/index.js
+++ week9/index.js
@@ -1,23 +1,55 @@
-const axios = require('axios');
+'use strict' 
 
-const fetchData = async () => {
-  try {
-    let config = {
-      method: 'get',
-      maxBodyLength: Infinity,
-      url: 'https://api.api-ninjas.com/v1/facts?limit=1',
-      headers: { 
-        'X-Api-Key': 'y5TJQhHVNXPu02MMlvYPGw==Qha8QKpVf2zMq8dC'
-      }
-    };
+async function getPlanetFact() {
+
+  var myHeaders = new Headers();
+  myHeaders.append("X-Api-Key", "y5TJQhHVNXPu02MMlvYPGw==Qha8QKpVf2zMq8dC");
+
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+  const userInput = document.getElementById('userInput').value;
+  const api_endpoint = `https://api.api-ninjas.com/v1/planets?name=${userInput}`
+  const response = await fetch(api_endpoint,requestOptions);
+  const json_response = await response.json();
+
+  console.log(JSON.stringify(json_response));
+  let keys = Object.keys(json_response[0]);
+  let data = JSON.stringify(json_response);
+  return data;
+}
+
+async function showPlanetFact() {
+  const factResponse = await getPlanetFact();
+  const paragraphTag = document.getElementById("fact");
+  paragraphTag.textContent = factResponse;
+}
+
+async function getWeather() {
+
+  var myHeaders = new Headers();
+  myHeaders.append("X-Api-Key", "y5TJQhHVNXPu02MMlvYPGw==Qha8QKpVf2zMq8dC");
+
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+  const userWeather = document.getElementById('userWeather').value;
+  const api_endpoint = `https://api.api-ninjas.com/v1/weather?city=${userWeather}`
+  const response = await fetch(api_endpoint,requestOptions);
+  const json_response = await response.json();
+
+  console.log(JSON.stringify(json_response[0]));
   
-    const response = await axios.request(config);
-    console.log(JSON.stringify(response.data[0].fact));
-    let data = response.data[0].fact;
-    return data;
-  } catch (error) {
-    console.log(error.message);
-  }
-};
+  let data = JSON.stringify(json_response);
+  return data;
+}
 
-fetchData();
+async function showWeather() {
+  const factResponse = await getWeather();
+  const paragraphTag = document.getElementById("weather");
+  paragraphTag.textContent = factResponse;
+}
