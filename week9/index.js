97d60cf579c245b8a18d8a9a3c70edf0821247ce
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-05-31 10:24:47 -0700

    commit

--- week9/index.js
+++ week9/index.js
@@ -15,7 +15,7 @@ async function getPlanetFact() {
   const response = await fetch(api_endpoint,requestOptions);
   const json_response = await response.json();
 
-  console.log(JSON.stringify(json_response));
+  //console.log(JSON.stringify(json_response));
   let keys = Object.keys(json_response[0]);
   let data = JSON.stringify(json_response);
   return data;
